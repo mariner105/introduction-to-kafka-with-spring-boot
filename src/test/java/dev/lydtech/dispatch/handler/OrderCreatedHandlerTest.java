d5ea60a63836b562ae36ecffb460866e1da1610f
Frank Hundley <mariner105@msn.com>
2023-09-23 17:29:46 -0700

    Added OrderCreatedHandler

---
+++ src/test/java/dev/lydtech/dispatch/handler/OrderCreatedHandlerTest.java
@@ -0,0 +1,26 @@
+package dev.lydtech.dispatch.handler;
+
+import dev.lydtech.dispatch.service.DispatchService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class OrderCreatedHandlerTest {
+
+    private OrderCreatedHandler handler;
+    private DispatchService dispatchServiceMock;
+
+    @BeforeEach
+    void setUp() {
+        dispatchServiceMock = mock(DispatchService.class);
+        handler = new OrderCreatedHandler(dispatchServiceMock);
+    }
+
+    @Test
+    void listen() {
+        handler.listen("payload");
+        verify(dispatchServiceMock, times(1)).process("payload");
+    }
+}
---
+++ src/test/java/dev/lydtech/dispatch/service/DispatchServiceTest.java
@@ -0,0 +1,21 @@
+package dev.lydtech.dispatch.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DispatchServiceTest {
+
+    private DispatchService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new DispatchService();
+    }
+
+    @Test
+    void process() {
+        service.process("payload");
+    }
+}
